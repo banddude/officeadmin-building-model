f0ec1a0cda5e98c32f594d95616ca890fc7a2aa7
banddude <127166397+banddude@users.noreply.github.com>
2026-09-17 15:36:01 -0700

    Export deterministic routing API

--- src/oabm/routing/__init__.py
+++ src/oabm/routing/__init__.py
@@ -1,1 +1,5 @@
-"""routing workstream."""
+"""Deterministic 3D electrical routing workstream."""
+
+from .router import NoRouteError, RoutingError, RoutingOptions, route_between_ports
+
+__all__ = ["NoRouteError", "RoutingError", "RoutingOptions", "route_between_ports"]
